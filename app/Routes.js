5955f1255057ff1209b0743a79372e8548ed8668
Marcos H <Cosmitar@users.noreply.github.com>
2017-03-02 23:54:25 -0300

    Update Routes.js
    Update use of react-router

--- app/Routes.js
+++ app/Routes.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 
 import Home from './containers/Home';
 
+const routes = {
+  path: '/',
+  indexRoute: { component: Home },
+  childRoutes: [
+    { path: '*', component: Home },
+  ],
+};
+
 const Routes = ({ store }) => (
   <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/">
-        <IndexRoute component={Home}/>
-      </Route>
-      <Route path="*" component={Home}/>
-    </Router>
+    <Router history={hashHistory} routes={ routes } />
   </Provider>
 );
-
 
 export default Routes;
